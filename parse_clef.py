1b0092580e2c77503c0caf99e0975b6b34edd1c7
paul <pldazl@gmail.com>
2015-10-22 22:42:48 +0100

    more parser work

--- parse_clef.py
+++ parse_clef.py
@@ -7,8 +7,8 @@ from whoosh.index import create_in
 from whoosh.analysis import StemmingAnalyzer
 from whoosh.fields import *
 
-#path = "./clef_small_sample/" #local path to single desktop file
-path = "./clef2015-sample/" #local path to desktop files
+path = "./clef_small_sample/" #local path to single desktop file
+#path = "./clef2015-sample/" #local path to desktop files
 html_files = os.listdir(path)
 stemmer = StemmingAnalyzer()
 
@@ -21,25 +21,24 @@ schema = Schema(docid=TEXT(stored=True),
 
 
 def parse_clef_files(html_file):
-    soup = BeautifulSoup(open(html_file, 'r').read(), "lxml")
     ndocid = html_file
+    soup = BeautifulSoup(open(path+html_file, 'r').read(), "lxml")
     print ndocid
     if soup.source is not None:
-        print "\n source = " + soup.source
+        print "\n source = "
+        print soup.source
     if soup.title is not None:
         ntitle = soup.title.string
-        print "title = " + ntitle
-    else:
-        print " \n " + ndocid+ "HAS NO TITLE! \n"
-
-    nalltext = soup.find_all(text=True)
-
-    print nalltext
+        print ntitle
+    if soup.find({""}) is not None:
+        print soup.timedate
+    ncontent = soup.findAll(text=True)
+    print ncontent
+    nalltext = soup.get_text()
 
 
 for html_file in html_files:
-
-    parse_clef_files(path+html_file)
+    parse_clef_files(html_file)
     print "NEXT FILE\n"
 
 
