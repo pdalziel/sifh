0c72cc6b5bdc6df19cafbbdb6d0e310ff8d409ad
paul <pldazl@gmail.com>
2015-10-28 21:26:12 +0000

    parser work

--- parse_clef.py
+++ parse_clef.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 from whoosh.index import create_in
 from whoosh.analysis import StemmingAnalyzer
 from whoosh.fields import *
+from whoosh.query import *
+from whoosh.qparser import QueryParser
 
 path = "./clef_small_sample/" #local path to single desktop file
 #path = "./clef2015-sample/" #local path to desktop files
@@ -20,22 +22,34 @@ schema = Schema(docid=TEXT(stored=True),
                 source=TEXT(stored=True),
                 alltext=TEXT(stored=True))
 
+if not os.path.exists("indexdir"):
+    os.mkdir("indexdir")
+
+# whoosh variables
+ix = create_in("indexdir", schema)
+writer = ix.writer()
+searcher = ix.searcher()
+qp = QueryParser("content", schema=ix.schema)
+q = qp.parse(u" ")
 
 def parse_files(html_file):
-    ndocid = html_file
+    schema.docid = html_file
     soup = BeautifulSoup(open(path+html_file, 'r').read(), "lxml")
-    print ndocid
     if soup.title is not None:
-        ntitle = soup.title.string
-        print ntitle
+        schema.title = soup.title.string
     [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
-    ncontent = soup.getText()
-    print ncontent
-    nalltext = ntitle + ncontent
+    schema.content = soup.getText()
+    schema.alltext = schema.title + schema.content
 
 
-for html_file in html_files:
-    parse_files(html_file)
-    print "NEXT FILE\n"
+def create_index():
+    for html_file in html_files:
+        parse_files(html_file)
+        print schema
+        writer.add_document(schema)
 
+writer.commit(optimize=True)
 
+with ix.searcher() as searcher:
+    results = searcher.search(q)
+    print len(results)
