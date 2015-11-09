6fb8a62b6c0d61b793cc243d533c5d03e71e9202
paul <pldazl@gmail.com>
2015-11-09 20:55:04 +0000

    added test skeleton

---
+++ tests/test_sifh.py
@@ -0,0 +1,22 @@
+__author__ = 'paul'
+import unittest
+
+
+class TestBlock(unittest.TestCase):
+    def test_parsing(self):
+        self.failUnless(False)
+
+    def test_block_content_extraction(self):
+        self.failUnless(False)
+
+    def test_index_creation(self):
+        self.failUnless(False)
+
+    def test_query_generation(self):
+        self.failUnless(False)
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
