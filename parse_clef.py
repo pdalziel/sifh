53f08101a93ea4dfdac798348bdd4c3c2cb4d8be
paul <pldazl@gmail.com>
2015-11-15 00:35:59 +0000

    added goose

--- parse_clef.py
+++ parse_clef.py
@@ -3,7 +3,7 @@ import os.path
 
 
 from bs4 import BeautifulSoup
-
+from goose import Goose
 from whoosh.index import create_in
 from whoosh.analysis import StemmingAnalyzer
 from whoosh.fields import *
@@ -31,20 +31,32 @@ writer = ix.writer()
 def parse_files(html_file):
     ndocid = html_file
     soup = BeautifulSoup(open(path+html_file, 'r').read(), "lxml")
+    print soup
     if soup.title is not None:
         ntitle = soup.title.string
-    [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
-    ncontent = soup.getText()
+
+    #[s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
+    ncontent = extract_content_block(soup)
     nalltext = ntitle + ncontent
+    print nalltext
     writer.add_document(docid=ndocid, title=ntitle, content=ncontent, alltext=nalltext)
 
 
 def create_index():
     for html_file in html_files:
-        parse_files(html_file)
-    writer.commit()
+        #parse_files(html_file)
+        extract_content_block(html_file)
+    #writer.commit()
 
+
+def extract_content_block(html_file):
+    g = Goose()
+    url = path+html_file
+    print url
+    article = g.extract(url=url)
+    content_block = article.cleaned_text[:150]
+    print content_block
 
 create_index()
-print ix
+
 
