1279e7b21f06c1203593912b494fe9fa9818af2d
paul <pldazl@gmail.com>
2015-11-17 21:16:25 +0000

    fixed unicode errors, added main(), restructured project

--- sifh/parse_clef.py
+++ sifh/parse_clef.py
@@ -1,19 +1,27 @@
 import os
 import os.path
-import codecs
-
-from bs4 import BeautifulSoup
-from goose import Goose
-from libextract.api import extract
+import sys
+import logging
 
 from whoosh import index
 from whoosh.index import create_in
 from whoosh.analysis import StemmingAnalyzer
 from whoosh.fields import Schema, TEXT, ID
 
+from bs4 import BeautifulSoup
+from goose import Goose
+from libextract.api import extract
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+logging.basicConfig(filename='sifh.log', level=logging.DEBUG)
+
 path = "./clef_small_sample/" #local path to single desktop file
 #path = "./clef2015-sample/" #local path to desktop files
+
 html_files = os.listdir(path)
+print html_files
 
 stemmer = StemmingAnalyzer()
 schema = Schema(docid=TEXT(stored=True),
@@ -32,23 +40,26 @@ if not os.path.exists("index"):
 
 def create_index():
     for html_file in html_files:
-        ndocid = html_file
-        with codecs.open(path+html_file, 'r', "utf-8") as f:
-            html = f.read()
-            g = Goose()
-            article = g.extract(raw_html=html)
-            ncontent = article.cleaned_text
-            if article.title is not None:
-                ntitle = article.title
-                print ntitle
-            print extract_all_text(html)
-            # writer.add_document(docid=ndocid, title=ntitle, content=ncontent)
-    # writer.commit()
+        ndocid = unicode(html_file)
+        html = open(path+html_file, 'r').read()
+        nalltext = extract_all_text(html)
+        g = Goose()
+        article = g.extract(raw_html=html)
+        ncontent = article.cleaned_text
+        if article.title is not None:
+            ntitle = article.title
+        writer.add_document(docid=ndocid, title=ntitle, content=ncontent, alltext=nalltext)
+    writer.commit()
 
 
 def extract_all_text(html):
     textnodes = list(extract(html))
     text_str = ''.join((e.text_content().encode('utf-8')) for e in textnodes)
-    return text_str.strip('  ')
+    return unicode(text_str.strip('  '))
 
-create_index()
+
+def main(argv):
+    create_index()
+
+if __name__ == "__main__":
+    main(sys.argv)
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,6 @@
 import os
 
+
 def asset_path(filename, asset_dir='assets'):
     return os.path.join(os.path.dirname(__file__),
                         asset_dir,
