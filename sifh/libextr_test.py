5e70aeb241d8742ca704b1e222b6b51cf6f555da
paul <pldazl@gmail.com>
2015-11-17 21:16:25 +0000

    fixed unicode errors, added main(), restructured project

--- sifh/libextr_test.py
+++ sifh/libextr_test.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+import sys
 
 from libextract.api import extract
 from goose import Goose
@@ -10,11 +11,24 @@ import nltk
 path = "./clef_small_sample/" #local path to single desktop file
 #path = "./clef2015-sample/" #local path to desktop files
 file_list = os.listdir(path)
-for f in file_list:
-    html = open(path+f, 'r').read()
-    g = Goose()
-    article = g.extract(raw_html=html)
-    # print article.cleaned_text
+def foo():
+    for f in file_list:
+        html = open(path+f, 'r').read()
+        print_nodes(html)
+        g = Goose()
+        article = g.extract(raw_html=html)
+        # print article.cleaned_text
+
+
+def print_nodes(html):
     textnodes = list(extract(html))
     text_str = ''.join((e.text_content().encode('utf-8')) for e in textnodes)
     print text_str
+
+
+
+def main(argv):
+    foo()
+
+if __name__ == "__main__":
+    main(sys.argv)
