49170aaa60f1c4caee358c8cc64639030f73262f
pldalzl <pldalzl@gmail.com>
2015-12-31 15:03:32 +0000

    added  highlight printing on hit in searcher

--- sifh/libextract_text.py
+++ sifh/libextract_text.py
@@ -4,4 +4,5 @@ from libextract.api import extract
 def extract_visable_text(html_path):
     textnodes = list(extract(html_path))
     text_str = unicode(''.join((e.text_content().encode('utf-8')) for e in textnodes))
+    print text_str
     return text_str
--- sifh/searcher.py
+++ sifh/searcher.py
@@ -7,22 +7,27 @@ from whoosh import index
 from whoosh import searching
 from whoosh.qparser import QueryParser
 
-#path = u"./clef_small_sample/" #local path to single desktop file
+path = "../clef_small_sample/" #local path to single desktop file
 #path = u"../clef2015/" #local path to desktop files
-path = u"../clef2015-problemfiles/"
+#path = u"../clef2015-problemfiles/"
 
 idx_name = path[3:-1]
+print idx_name
 
-
-path_to_index = "/home/paul/Project/sifh/index"
+path_to_index = "/home/paul/Project/sifh/sifh/index"
 ix = index.open_dir(path_to_index, indexname=idx_name)
 
 #searcher = ix.searcher()
 qp = QueryParser("content", schema=ix.schema)
-q = qp.parse(u"psychotherapy")
+q = qp.parse(u"health")
 
 with ix.searcher() as searcher:
     results = searcher.search(q, terms=True)
-    for hit in results:
-        print(hit["title"])
-        print(hit.highlights("content"))
+    if len(results) > 0:
+        for hit in results:
+            print(hit["docid"])
+            print(hit["title"])
+            print(hit.highlights("content"))
+    else:
+        print "no hits"
+
