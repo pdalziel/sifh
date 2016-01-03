7e7aba16b343b43e34318429184e716ee2a57e34
paul <pldazl@gmail.com>
2016-01-03 16:29:47 +0000

    searcher parses queries and logs results

--- sifh/searcher.py
+++ sifh/searcher.py
@@ -1,6 +1,7 @@
 import os
 import os.path
-
+import xml.etree.ElementTree as ET
+import logging
 
 from whoosh import query
 from whoosh import index
@@ -8,21 +9,31 @@ from whoosh import searching
 from whoosh.qparser import QueryParser
 
 #path = u"./clef_small_sample/" #local path to single desktop file
-#path = u"../clef2015/" #local path to desktop files
-path = u"../clef2015-problemfiles/"
+path = "../clef2015/" #local path to desktop files
+#path = u"../clef2015-problemfiles/"
+
+
 
 idx_name = path[3:-1]
-
-
+queries_path = "/home/paul/Project/sifh/queries/clef2015.training.queries-EN.txt" # "/home/paul/Project/sifh/queries/clef2015.test.queries-EN.txt"
+logging.basicConfig(filename='searcher.log', level=logging.INFO)
+log = logging.getLogger('logger')
 path_to_index = "/home/paul/Project/sifh/index"
-ix = index.open_dir(path_to_index, indexname=idx_name)
+ix = index.open_dir(path_to_index)
 
-#searcher = ix.searcher()
 qp = QueryParser("content", schema=ix.schema)
-q = qp.parse(u"psychotherapy")
 
-with ix.searcher() as searcher:
-    results = searcher.search(q, terms=True)
-    for hit in results:
-        print(hit["title"])
-        print(hit.highlights("content"))
+tree = ET.parse(queries_path)
+root = tree.getroot()
+
+for q in root.findall('top'):
+    query = q.find('query').text
+    q = qp.parse(unicode(query))
+    with ix.searcher() as searcher:
+        results = searcher.search(q, terms=True)
+        #log.info(str(len(results)) + " hits")
+        log.info(query)
+        print query
+        for hit in results:
+                log.info(u"TITLE: " + (hit["title"]) + u" DOCID: " + (hit["docid"]) + u" SCORE:  " + unicode(hit.score))
+                print (u"TITLE: " + (hit["title"]) + u" DOCID: " + (hit["docid"]) + u" SCORE: " + unicode(hit.score))
