4fd5c6dfa43dde3b5cfeda01f8a069929096b02e
paul <pldazl@gmail.com>
2016-01-05 14:41:52 +0000

    git issues

--- sifh/searcher.py
+++ sifh/searcher.py
@@ -9,6 +9,7 @@ from whoosh import searching
 from whoosh.qparser import QueryParser
 
 #path = u"./clef_small_sample/" #local path to single desktop file
+
 path = "../clef2015/" #local path to desktop files
 #path = u"../clef2015-problemfiles/"
 
@@ -37,3 +38,20 @@ for q in root.findall('top'):
         for hit in results:
                 log.info(u"TITLE: " + (hit["title"]) + u" DOCID: " + (hit["docid"]) + u" SCORE:  " + unicode(hit.score))
                 print (u"TITLE: " + (hit["title"]) + u" DOCID: " + (hit["docid"]) + u" SCORE: " + unicode(hit.score))
+
+#path = u"../clef2015/" #local path to desktop files
+path = u"../clef2015-problemfiles/"
+
+idx_name = path[3:-1]
+
+
+path_to_index = "/home/paul/Project/sifh/index"
+ix = index.open_dir(path_to_index, indexname=idx_name)
+
+
+with ix.searcher() as searcher:
+    results = searcher.search(q, terms=True)
+    for hit in results:
+        print(hit["title"])
+        print(hit.highlights("content"))
+
