097fa32682ef6ec855e9e75c066bbaf543290c53
paul <pldazl@gmail.com>
2016-01-25 07:03:55 +0000

    fixed output format of results file generation

--- sifh/searcher.py
+++ sifh/searcher.py
@@ -8,10 +8,10 @@ from whoosh import index
 from whoosh import searching
 from whoosh.qparser import QueryParser
 
-#path = u"./clef_small_sample/" #local path to single desktop file
+# path = u"./clef_small_sample/" #local path to single desktop file
 
-path = "../clef2015/" #local path to desktop files
-#path = u"../clef2015-problemfiles/"
+path = "../clef2015/"  # local path to desktop files
+# path = u"../clef2015-problemfiles/"
 
 res_file = "./res_file.txt"
 
@@ -24,16 +24,21 @@ path_to_index = "/home/paul/Project/sifh/index"
 ix = index.open_dir(path_to_index)
 
 qp = QueryParser("content", schema=ix.schema)
-#f = open(queries_path)
+# f = open(queries_path)
 
 tree = ET.parse(queries_path)
 root = tree.getroot()
-iter = "Q0"
+itr = " 0 "
+run_id = " base "
+rank = " 0 "
 
 
-def create_results_flie(qid, docid, iter, rank ):
+def create_results_flie(qid, docid, sim):
     with open(res_file, 'a') as result_file:
-        result_file.write(qid + " " + docid + " " + iter + " " + rank + "\n")
+        pass
+        result_file.write(str(qid + itr + docid + rank + sim + run_id + "\n"))
+        # print (str(qid + itr + docid + rank + sim + run_id + "\n"))
+
 
 for q in root.findall('top'):
     # print q
@@ -41,24 +46,19 @@ for q in root.findall('top'):
     qid = str(q.find('num').text)
     q = qp.parse(unicode(query))
     with ix.searcher() as searcher:
-        results = searcher.search(q, terms=True)
-        log.info(str(len(results)) + " hits")
+        results = searcher.search(q, limit=None)
+        # log.info(str(len(results)) + " hits")
         log.info(query)
-        print query
-        if results.has_matched_terms:
-            rank = str(results.score(0))
-        else:
-            rank = "0"
+        #print query
+
+        #print results.__len__()
         for hit in results:
-                docid = str(hit["docid"])
-                log.info(u"TITLE: " + (hit["title"]) + u" DOCID: " + (hit["docid"]) + u" SCORE:  " + unicode(hit.score))
-                # print (hit.matched_terms())
-                # print (u"TITLE: " + (hit["title"]) + u" DOCID: " + (hit["docid"]) + (hit["meta_tag_contents"]) + u" SCORE: " + unicode(hit.score))
-    create_results_flie(qid, iter, docid, rank)
-
-
-
-
-
-
-
+            if results.has_matched_terms:
+                sim = str(hit.score)
+            else:
+                sim = "0"
+            docid = str(hit["docid"])
+            create_results_flie(qid, docid, sim)
+            # log.info(u"TITLE: " + (hit["title"]) + u" DOCID: " + (hit["docid"]) + u" SCORE:  " + unicode(hit.score))
+            # print (hit.matched_terms())
+            # print (u"TITLE: " + (hit["title"]) + u" DOCID: " + (hit["docid"]) + (hit["meta_tag_contents"]) + u" SCORE: " + unicode(hit.score))
